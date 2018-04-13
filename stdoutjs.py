8cfad47b65540a4a54f1e641543302221c55674e
Destin Moulton <destin.moulton@gmail.com>
2018-04-13 14:55:43 -0600

    Added window.onload method.

--- stdoutjs.py
+++ stdoutjs.py
@@ -1,8 +1,16 @@
 STDOUTJS = """
+(function(){
+    "use strict";
 
-function scroll(id){
-    var element = document.getElementById(id);
-    element.scrollTop = element.scrollHeight - element.clientHeight; 
-    console.log("Scrolling...", id);
-}
+    window.onload = function(){
+        console.log("loaded");
+    }
+
+    function scroll(id){
+        var element = document.getElementById(id);
+        element.scrollTop = element.scrollHeight - element.clientHeight; 
+        console.log("Scrolling...", id);
+    }
+})()
+
 """
