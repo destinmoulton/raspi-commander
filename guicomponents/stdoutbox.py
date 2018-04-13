589074fc5f35ca8c6a7e0174e4a8cb233d8a416e
Destin Moulton <destin.moulton@gmail.com>
2018-04-13 14:55:58 -0600

    Moved the js container to the the lower function.

--- guicomponents/stdoutbox.py
+++ guicomponents/stdoutbox.py
@@ -9,15 +9,11 @@ class StdoutBox:
         self.stdouts = []
         self.jsCounter = 0
 
-        self.js = gui.Tag(_type='script')
-        self.js.add_child("STDOUTJS", STDOUTJS)
-
         self.hidden_stdout = gui.TextInput(
             single_line=False, id="hidden_stdout")
         self.hidden_stdout.style["display"] = "none"  # Hide this box
 
-        self.lst_termout = gui.ListView(
-            style=StdoutBoxStyles["list"], id="stdout")
+        self.lst_termout = gui.ListView(style=StdoutBoxStyles["list"])
 
     def append(self, data):
         self.stdouts.append(data)
@@ -34,10 +30,13 @@ class StdoutBox:
         hbox_top.append(lb_title)
         hbox_top.append(bt_clear)
 
+        js = gui.Tag(_type='script')
+        js.add_child("STDOUTJS", STDOUTJS)
+
         vbox_stdout = gui.VBox(style=StdoutBoxStyles["stdout_section"])
         vbox_stdout.append(hbox_top)
         vbox_stdout.append(self.lst_termout)
-        vbox_stdout.add_child("scrolltobottom", self.js)
+        vbox_stdout.add_child("scrolltobottom", js)
         return vbox_stdout
 
     def on_click_clear(self, widget):
