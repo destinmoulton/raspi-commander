a90fe80c47bf1134d0f639e1d71eb684e2b6964e
Destin Moulton <destin.moulton@gmail.com>
2018-04-18 17:12:33 -0600

    Renamed lastrefreshed.py to refresh_bar.py and rebuilt.

---
+++ guicomponents/refresh_bar.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+import remi.gui as gui
+
+from guicomponents.generic_refresh_button import GenericRefreshButton
+from Styles import LastRefreshedStyles
+
+
+class RefreshBar:
+    def __init__(self, refresh_handler):
+        self.refresh_handler = refresh_handler
+
+    def build_refresh_bar(self):
+        generic_refresh_button = GenericRefreshButton()
+        refresh_button = generic_refresh_button.build_button(
+            "Refresh", self.run_refresh)
+
+        self.box_last_refreshed = gui.Widget()
+
+        hbox_refresh_bar = gui.HBox()
+        hbox_refresh_bar.append(refresh_button)
+        hbox_refresh_bar.append(self.box_last_refreshed)
+        return hbox_refresh_bar
+
+    def run_refresh(self):
+        self.refresh_handler()
+
+    def update_refresh_time(self):
+        # Update the refresh time
+        self.box_last_refreshed.empty()
+        self.box_last_refreshed.append(
+            self.get_last_refreshed())
+
+    def get_last_refreshed(self):
+
+        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        text = "Last refreshed: {}".format(time)
+
+        lb = gui.Label(text, style=LastRefreshedStyles["text"])
+
+        cont = gui.Widget()
+        cont.append(lb)
+        return cont
