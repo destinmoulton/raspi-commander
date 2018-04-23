f48022966dbc587b003efe4f465a011b8cbddba3
Destin Moulton <destin.moulton@gmail.com>
2018-04-23 14:20:52 -0600

    Added methods to create the modal and toggle the modal animation class.

--- res/raspicommander.js
+++ res/raspicommander.js
@@ -4,11 +4,40 @@
     let currentCount = 0;
 
     window.onload = function() {
+        console.log("RUNNING!");
+        addCSS("res/raspicommander.css");
+        addModalElement();
+        toggleLoadingModal(true);
         setInterval(function() {
             // Check whether the scroll needs to change
             shouldScroll();
         }, 100);
     };
+
+    function addModalElement() {
+        const modal = document.createElement("div");
+        modal.setAttribute("class", "modal");
+
+        const loadingAnimation = document.createElement("div");
+        loadingAnimation.setAttribute("class", "loading-animation");
+        modal.appendChild(loadingAnimation);
+
+        document.body.appendChild(modal);
+    }
+
+    function toggleLoadingModal(state) {
+        const className = state ? "loading" : "";
+        document.body.setAttribute("class", className);
+    }
+
+    function addCSS(fileName) {
+        const css = document.createElement("link");
+        css.setAttribute("rel", "stylesheet");
+        css.setAttribute("type", "text/css");
+        css.setAttribute("href", fileName);
+
+        document.head.appendChild(css);
+    }
 
     function shouldScroll() {
         const el = document.getElementById("stdoutlist");
