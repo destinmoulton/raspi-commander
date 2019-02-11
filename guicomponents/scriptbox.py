685c443f51970c57ce24ce407ce6c8168c8c872f
Destin Moulton <destin.moulton@gmail.com>
2019-02-11 14:11:41 -0700

    Removed strong tag

--- guicomponents/scriptbox.py
+++ guicomponents/scriptbox.py
@@ -57,7 +57,7 @@ class ScriptBox:
         now = timehelper.get_now_formatted()
         # Update the label
         self.lb_lastrun_script.set_text(
-            "<strong>Last run:</strong> {} at {}".format(script["name"], now))
+            "Last run: `{}` at {}".format(script["name"], now))
 
     def _run_cmd(self, cmd):
         """Run a command"""
